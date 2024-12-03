d2444e6406e0e2c14c0d064d25c7caa8dd0cf105
jhoner guzman <jhonerguzmana@uparsistem.edu.co>
2024-12-03 08:12:29 -0500

    laboratorio entregable #4

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 console.log("Estoy en JS")
+let $resultado = document.getElementById("result")
+function fSumar(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2. value)
+    // console. log(typeof(num1))
+    let resultado = "La suma es: "* (num1+num2)
+    console. log(typeof(num1))
+    $resultado.innerHTML = "la suma es: "+ (num1+num2)
+}
+    function fRestar(){
+    let  num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    console. log("La resta es: "* (num1-num2))
+    $resultado.innerHTML = "la resta es: "+ (num1+num2)
+ }
+    I
+    function fMultiplicar(){
+    let numl = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    console.log("La multiplicacion es: "+ (num1*num2))
+    $resultado.innerHTML = "la multiplicacion es: "+ (num1+num2)
+}
+    function fDividir(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    if(num2===0)
+    {console. log("La division entre cero (0) No es posible")} 
+    $resultado.innerHTML = "la division es: "+ (num1+num2)
+    else{console.log("la division es : "num1/num2))
+
+     }
